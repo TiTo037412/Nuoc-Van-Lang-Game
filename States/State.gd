c2ed4c2813ac972ab8f95fc2d8b7521a3ba7c48a
TiTo037412 <anhhuy.bao.nguyen@gmail.com>
2023-10-21 20:13:40 +0700

    state machine

---
+++ States/State.gd
@@ -0,0 +1,16 @@
+extends Node
+class_name State
+
+signal Transitioned
+# Called when the node enters the scene tree for the first time.
+func Enter():
+	pass
+
+func Exit():
+	pass
+
+func Update(_delta: float):
+	pass
+	
+func Physic_Update(_delta: float):
+	pass
