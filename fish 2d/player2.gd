aa6866e2405ae9a285b690a33332a0c3f5c53066
Cheese <104724710+Cheesitive@users.noreply.github.com>
2023-11-12 14:14:36 +0700

    Add files via upload

---
+++ fish 2d/player2.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	var directions = Input.get_vector("left", "right", "up", "down")
+	position += directions*_delta*100
